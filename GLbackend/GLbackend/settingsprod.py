8adc4dafab664df33ad32bd1936a85439381a6d5
casxai <joeynicole99@gmail.com>
2023-12-05 23:29:27 +0800

    build for production

--- GLbackend/GLbackend/settingsprod.py
+++ GLbackend/GLbackend/settingsprod.py
@@ -60,12 +60,13 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 
+
 CORS_ALLOWED_ORIGINS = [
-    "http://gaminglounge.com","165.22.242.30"
+    'http://api.gaminglounge.com','http://gaminglounge.com','http://165.22.242.30','http://localhost','http://127.0.0.1'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://gaminglounge.com","165.22.242.30"
+    'http://api.gaminglounge.com','http://gaminglounge.com','http://165.22.242.30','http://localhost','http://127.0.0.1'
 ]
 
 INSTALLED_APPS = [
@@ -128,7 +129,16 @@ DATABASES = {
         "PORT": "",
     }
 }
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME":  "gaminglounge",
+#         "USER": "gluser",
+#         "PASSWORD": "adminadmin02",
+#         "HOST": "localhost",
+#         "PORT": "",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
