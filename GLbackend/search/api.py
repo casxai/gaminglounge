93095f79f216b882c770e5d08a4a78583b0dad98
casxai <joeynicole99@gmail.com>
2023-12-12 11:17:49 +0800

    dumpdata

--- GLbackend/search/api.py
+++ GLbackend/search/api.py
@@ -21,7 +21,7 @@ def search(request):
     users = User.objects.filter(name__icontains=query)
     users_serializer = UserSerializer(users, many=True)
     
-    posts = Post.objects.filter(Q(body__icontains=query, is_private=False) | Q(created_by_id__in=list(user_ids), body__icontains=query)) 
+    posts = Post.objects.filter(Q(body__icontains=query, is_private=False, is_offensive=False) | Q(created_by_id__in=list(user_ids), body__icontains=query)) 
     
     posts_serializer = PostSerializer(posts, many=True)
     
